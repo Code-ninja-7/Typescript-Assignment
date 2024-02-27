2a9d6e38b1dbd36c652a1b8c5b0d522a906927b9
Code-ninja-7 <161529169+Code-ninja-7@users.noreply.github.com>
2024-02-27 15:50:22 -0800

    Add files via upload

---
+++ 12 Greetings.ts
@@ -0,0 +1,10 @@
+
+
+let names : string[] = ['Esha','Nimra','Ali','Ahmed'];
+const masseg :string = 'wellcome to the Typescript class :  ' 
+
+for(let i=0; i<names.length;i++){
+
+    console.log(masseg + names[i]);
+    
+}
